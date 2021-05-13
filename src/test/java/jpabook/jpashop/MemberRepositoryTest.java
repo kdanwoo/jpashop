8f2c8eb84b2c68a0d2d0fa3610dc7e75cfebf015
danwook <k.danwoo91@ibksystem.co.kr>
2021-05-13 14:04:09 +0900

    JPA 프로젝트

---
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,39 @@
+package jpabook.jpashop;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.validation.constraints.Size;
+
+import static org.junit.Assert.*;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional //test가 끝난후에 ROLLBACK 시킴~
+    public void testMember() throws Exception {
+        Member member = new Member();
+        member.setUsername("memberA");
+        
+        Long savedID = memberRepository.save(member);
+        Member findMember = memberRepository.find(savedID);
+
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+
+
+    }
+
+
+}
